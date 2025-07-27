6e0a65990e5ee8f588899f56a0ea1d9c06debde5
Neeraj chauhan <nc1676639@gmail.com>
2025-07-27 23:11:49 +0530

    login-logout

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,6 +1,7 @@
 const user=require("../models/userModel")
 const bcrypt=require("bcrypt")
 const jwt=require("jsonwebtoken")
+const cookie =require("cookie-parser")
 
 exports.register=async(req,res)=>{
    const {username,email,password}=req.body;
@@ -34,7 +35,50 @@ exports.register=async(req,res)=>{
     
    }
    
-  
-   
 
+}
+
+
+exports.loginControllers=async(req,res)=>{
+
+  const {email,password}=req.body;
+
+  try {
+
+    const User=await user.findOne({email})
+    if(!User){
+      return res.status(400).json({error:"invalid credential"})
+    }
+
+    //password match
+    const isMatch=await bcrypt.compare(password, User.password) 
+
+    if(!isMatch){
+     return res.status(400).json({error:"password is not match"})
+    }
+  
+
+    const token=jwt.sign({id:User._id},
+      process.env.JWT_ACCESS_SECRET,{
+        expiresIn:"1h"
+      })
+
+      res.cookie("token",token)
+      res.status(201).json({message:"login successfully",
+        email:User.email,
+        password:User.password
+      })
+
+  
+
+  } catch (error) {
+    res.status(500).json({error:"error in login server"})
+    
+  }
+}
+
+
+exports.logoutControllers=async(req,res)=>{
+  res.clearCookie("token")
+  res.status(201).json({message:"logout successfully"})
 }
